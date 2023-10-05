62fae4e678a2f685266c10122765219a4afdf691
alexsandrosr88 <alexsandrosr@msn.com>
2023-10-05 00:10:24 -0300

    Concluído o desafio 3

---
+++ src/stream_api/Desafio3.java
@@ -0,0 +1,19 @@
+package stream_api;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Desafio3 {
+    public static void main(String[] args){
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        boolean numerosPositivos = numeros.stream()
+                .allMatch(n-> n >0);
+
+        if(numerosPositivos)
+            System.out.println("\nTodos os números são positivos.");
+        else
+            System.out.println("\nTodos os números não são positivos.");
+    }
+}

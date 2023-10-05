8cc5e6db9e4a88de0dc893e8c56f19d9872e7aa4
alexsandrosr88 <alexsandrosr@msn.com>
2023-10-04 23:10:13 -0300

    Concluído o desafio 1

---
+++ src/stream_api/Desafio1.java
@@ -0,0 +1,15 @@
+package stream_api;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Desafio1 {
+    public static void main(String[] args){
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        numeros.stream()
+                .sorted(Comparator.naturalOrder())
+                .forEach(n -> System.out.print(n+ " "));
+    }
+}
